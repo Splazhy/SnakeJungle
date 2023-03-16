5e4ed1b0dd1f6d15c4cd2b041087c3f709f11c38
BTKbe <90573740+SystemCallBeam@users.noreply.github.com>
2023-03-16 16:09:46 +0700

    high score
    add save high score

--- Score.java
+++ Score.java
@@ -1,32 +1,26 @@
 
 public class Score {
-    private static int maxScore = 0; // test 0 score
+    private static int maxScore = HighScore.getHighScore();
     private static int curScore = 0;
 
-    public static void inScore(int score){
+    public static void addScore(int score){
         curScore += score;
         if (maxScore < curScore) {
             maxScore = curScore;
         }
     }
 
-    public static void inScore(){
-        curScore += 50;
-        if (maxScore < curScore) {
-            maxScore = curScore;
-        }
-    }
-
     public static void reScore(int score){
+        if (maxScore > HighScore.getHighScore()) HighScore.setHighScore(String.valueOf(maxScore));
         curScore = 0;
     }
 
     public static String MAX_SCORE() {
-        return String.format("[maxScore : %d]", maxScore);
+        return String.format("[High Score : %d]", maxScore);
     }
 
     public static String CUR_SCORE() {
-        return String.format("[maxScore : %d]", curScore);
+        return String.format("[Score : %d]", curScore);
     }
 
     @Override
